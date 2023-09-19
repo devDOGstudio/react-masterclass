7a9aeba7c4f9313a14443084dba523b746fafa4d
dev_dog <devdogstudio@gmail.com>
2023-09-20 07:48:26 +0900

    6.10 finish

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,43 +1,130 @@
-import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
-/*
+interface IForm {
+  toDo: string;
+}
+
 function ToDoList() {
-  const [toDo, setToDo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDo(value);
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const handleValid = (data: IForm) => {
+    console.log("add to do", data.toDo);
+    setValue("toDo", "");
   };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} placeholder="to do 작성" />
+      <form onSubmit={handleSubmit(handleValid)}>
+        <input
+          {...register("toDo", {
+            required: "toDo를 작성해주세요.",
+          })}
+          placeholder="toDo"
+        />
         <button>Add</button>
+      </form>
+    </div>
+  );
+}
+
+/*
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  userName: string;
+  passWord: string;
+  passWord1: string;
+  extraError?: string;
+}
+
+function ToDoList() {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IForm>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
+  const onValid = (data: IForm) => {
+    if (data.passWord !== data.passWord1) {
+      setError(
+        "passWord1",
+        { message: "passWord are not the same" },
+        { shouldFocus: true }
+      );
+    }
+    //setError("extraError", { message: "server offline" });
+  };
+  //console.log(error);
+  return (
+    <div>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("email", {
+            required: "email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver\.com$/,
+              message: "only naver.com emails allowed",
+            },
+          })}
+          placeholder="email"
+        />
+        <span> {errors?.email?.message as string}</span>
+        <input
+          {...register("firstName", {
+            required: "firstName is required",
+            validate: {
+              noDevdog: async (value) =>
+                value.includes("devdog") ? "no devdog allowed" : true,
+              noDev_dog: (value) =>
+                value.includes("dev_dog") ? "no dev_dog allowed" : true,
+            },
+          })}
+          placeholder="firstName"
+        />
+        <span> {errors?.firstName?.message as string}</span>
+        <input
+          {...register("lastName", { required: "lastName is required" })}
+          placeholder="lastName"
+        />
+        <span> {errors?.lastName?.message as string}</span>
+        <input
+          {...register("userName", {
+            required: "userName is required",
+            minLength: { value: 5, message: "your userName is too Short" },
+          })}
+          placeholder="userName"
+        />
+        <span> {errors?.userName?.message as string}</span>
+        <input
+          {...register("passWord", {
+            required: "passWord is required",
+            minLength: { value: 5, message: "your passWord is too Short" },
+          })}
+          placeholder="passWord"
+        />
+        <span> {errors?.passWord?.message as string}</span>
+        <input
+          {...register("passWord1", {
+            required: "passWord1 is required",
+            minLength: { value: 5, message: "your passWord1 is too Short" },
+          })}
+          placeholder="passWord1"
+        />
+        <span> {errors?.passWord1?.message as string}</span>
+        <button>Add</button>
+        <span> {errors?.extraError?.message as string}</span>
       </form>
     </div>
   );
 }
 */
-
-function ToDoList() {
-  const { register, watch } = useForm();
-  console.log(watch());
-  return (
-    <div>
-      <form>
-        <input {...register("email")} placeholder="to do 작성" />
-        <input {...register("fn")} placeholder="to do 작성" />
-        <input {...register("ln")} placeholder="to do 작성" />
-        <button>Add</button>
-      </form>
-    </div>
-  );
-}
 
 export default ToDoList;
